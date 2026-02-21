e5eb620457cc84fff3d4234041a560f455a6cdcf
Sanat Devgan <sanatleo1985@gmail.com>
2026-02-21 23:45:37 +0530

    chore: clean repo and add proper .gitignore

--- contracts/first.sol
+++ contracts/first.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20; //solidity version
+pragma solidity ^0.8.24; //solidity version
 
 contract mystorage {
     uint256 public  favno1; // initialize with 0
